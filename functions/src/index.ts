8db64975a061ebbf840fef9c682c5d1c58fec8b0
Josh Sprow <jsprow@gmail.com>
2020-06-27 16:29:47 -0400

    Added some error handling to cloud functions.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,17 +5,29 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-exports.onUserCreate = functions.auth.user().onCreate((user) =>
-  db.doc(`users/${user.uid}`).create({
-    creationTime: user.metadata.creationTime,
-    displayName: user.displayName,
-    email: user.email,
-    lastSignInTime: user.metadata.lastSignInTime,
-    uid: user.uid,
-    x: user.providerData,
-  })
-);
+exports.onUserCreate = functions.auth.user().onCreate(async (user) => {
+  try {
+    await db.doc(`users/${user.uid}`).create({
+      creationTime: user.metadata.creationTime,
+      displayName: user.displayName,
+      email: user.email,
+      lastSignInTime: user.metadata.lastSignInTime,
+      uid: user.uid,
+      x: user.providerData,
+    });
 
-exports.onUserDelete = functions.auth
-  .user()
-  .onDelete((user) => db.doc(`users/${user.uid}`).delete());
+    return { success: true };
+  } catch (error) {
+    throw new functions.https.HttpsError("internal", error.message);
+  }
+});
+
+exports.onUserDelete = functions.auth.user().onDelete(async (user) => {
+  try {
+    await db.doc(`users/${user.uid}`).delete();
+
+    return { success: true };
+  } catch (error) {
+    throw new functions.https.HttpsError("internal", error.message);
+  }
+});
