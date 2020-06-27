7f165a22c84add5d994c0158c13bfa80aa2b634d
Josh Sprow <jsprow@gmail.com>
2020-06-27 16:42:51 -0400

    Trying different firestore methods to create user in db.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,31 +7,22 @@ const db = admin.firestore();
 
 exports.onUserCreate = functions.auth.user().onCreate(async (user) => {
   try {
-    await db.doc(`users/${user.uid}`).create({
+    await db.collection("users").doc(user.uid).set({
       creationTime: user.metadata.creationTime,
       displayName: user.displayName,
       email: user.email,
       lastSignInTime: user.metadata.lastSignInTime,
       uid: user.uid,
-      x: user.providerData,
     });
-
-    return { success: true };
   } catch (error) {
     console.error(error);
-
-    return { success: false };
   }
 });
 
 exports.onUserDelete = functions.auth.user().onDelete(async (user) => {
   try {
     await db.doc(`users/${user.uid}`).delete();
-
-    return { success: true };
   } catch (error) {
     console.error(error);
-
-    return { success: false };
   }
 });
