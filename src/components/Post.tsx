905e8ee90b7847525adaa9379877db5b9566448f
Josh Sprow <jsprow@gmail.com>
2020-06-28 18:42:45 -0400

    Delete post button is functional, and pretty.

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,13 +1,27 @@
 import {
-  ListItem,
-  ListItemText,
-  Typography,
-  ListItemAvatar,
   Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  ClickAwayListener,
+  IconButton,
+  IconButtonProps,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+  Popover,
+  Tooltip,
+  Typography,
 } from "@material-ui/core";
+import { Delete as DeleteIcon, Edit as EditIcon } from "@material-ui/icons";
 import React, { FC, useEffect, useState } from "react";
-import { PostDoc, UserDoc } from "../context";
+import { PostDoc, useAppContext, UserDoc } from "../context";
 import { db, formatTimestamp } from "../firebase";
+
+interface PostProps {
+  post: PostDoc;
+}
 
 const getPostUser = async (userId: string) => {
   const doc = await db.doc(`users/${userId}`).get();
@@ -15,10 +29,76 @@ const getPostUser = async (userId: string) => {
   return doc.data() as UserDoc;
 };
 
-const deletePost = async (postId: string) => db.doc(`posts/${postId}`).delete();
+const DeleteButton: FC<
+  Pick<PostProps, "post"> & {
+    setUpdating: React.Dispatch<React.SetStateAction<boolean>>;
+    updating: boolean;
+  }
+> = ({ post, setUpdating, updating }) => {
+  const [buttonEl, setButtonEl] = useState<HTMLButtonElement | null>(null);
 
-const Post: FC<{ post: PostDoc }> = ({ post }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleIconButton: IconButtonProps["onClick"] = (event) => {
+    setButtonEl(event.currentTarget);
+    setOpen(true);
+  };
+
+  const closePopover = () => setOpen(false);
+
+  const deletePost = async () => {
+    setUpdating(true);
+
+    db.doc(`posts/${post.postId}`).delete();
+
+    setUpdating(false);
+    closePopover();
+  };
+
+  return (
+    <>
+      <IconButton disabled={updating} onClick={handleIconButton} size="small">
+        <DeleteIcon fontSize="inherit" />
+      </IconButton>
+      <Popover
+        anchorEl={buttonEl}
+        anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
+        open={open}
+        transformOrigin={{ horizontal: "center", vertical: "top" }}
+      >
+        <ClickAwayListener onClickAway={closePopover}>
+          <Box
+            alignItems="center"
+            display="flex"
+            flexDirection="column"
+            padding="0.5rem"
+          >
+            <Typography variant="h6">Delete this post?</Typography>
+            <ButtonGroup>
+              <Button
+                color="secondary"
+                disabled={updating}
+                onClick={deletePost}
+                size="small"
+              >
+                Ok
+              </Button>
+              <Button disabled={updating} onClick={closePopover} size="small">
+                Cancel
+              </Button>
+            </ButtonGroup>
+          </Box>
+        </ClickAwayListener>
+      </Popover>
+    </>
+  );
+};
+
+const Post: FC<PostProps> = ({ post }) => {
+  const { userDoc } = useAppContext();
   const [postUser, setPostUser] = useState("...");
+
+  const [updating, setUpdating] = useState(false);
 
   useEffect(() => {
     getPostUser(post.userId)
@@ -37,6 +117,22 @@ const Post: FC<{ post: PostDoc }> = ({ post }) => {
         </Typography>
         <Typography variant="body1">{post.message}</Typography>
       </ListItemText>
+      <ListItemSecondaryAction>
+        {userDoc && userDoc.userId === post.userId && (
+          <>
+            <Tooltip title="Edit">
+              <IconButton disabled={updating} size="small">
+                <EditIcon fontSize="inherit" />
+              </IconButton>
+            </Tooltip>
+            <DeleteButton
+              post={post}
+              setUpdating={setUpdating}
+              updating={updating}
+            />
+          </>
+        )}
+      </ListItemSecondaryAction>
     </ListItem>
   );
 };
